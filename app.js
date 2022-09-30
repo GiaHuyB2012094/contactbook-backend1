b9abbfe86c00044472010bb900103af02e91fccf
Huy <huyDEVSE040902@gmail.com>
2022-09-30 08:41:51 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 app.get("/",(req,res) => {
     res.json({message : "Welcome to contact book application."});
+});
+
+app.use("/api/contacts",contactsRouter);
+
+app.use((req, res, next) => {
+    return next (new ApiError(404,"Resource not found"));
+})
+app.use((err, req, res, next) => {
+    return res.status(Error.statusCode || 500).json({
+        message: Error.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
